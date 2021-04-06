c92f40c2a66f543b217c110ae05e6d7db20aec29
GraysonP102 <78446269+GraysonP102@users.noreply.github.com>
2021-04-06 16:01:49 -0400

    Add second button
    Added second button. Had to change title of first button to do this.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
-function myFunction() {
+function whiteRice() {
   let x = document.getElementById("whiteRice");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function CaliRice() {
+  let x = document.getElementById("CaliRice");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
