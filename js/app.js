fef3dc78046937b7140ffb41079bb399d213d258
GraysonP102 <78446269+GraysonP102@users.noreply.github.com>
2021-04-06 15:56:32 -0400

    Add button
    Added a button to display text when clicked.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-prompt("What kind of rice do you want?")
+function myFunction() {
+  let x = document.getElementById("whiteRice");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
