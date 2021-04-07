ddccf4d87f4ea6922735686e0b5b27dac6338104
GraysonP102 <78446269+GraysonP102@users.noreply.github.com>
2021-04-07 17:58:10 -0400

    Replace " with `
    Replaced all inchmarks with backticks.

--- js/app.js
+++ js/app.js
@@ -1,45 +1,45 @@
-function whiteRice() {
-    let x = document.getElementById("whiteRice");
-    let y = document.getElementById("CaliRice");
-    y.style.display = "none";
-    if (x.style.display === "none") {
-        x.style.display = "block";
+  function whiteRice() {
+    let x = document.getElementById(`whiteRice`);
+    let y = document.getElementById(`CaliRice`);
+    y.style.display = `none`;
+    if (x.style.display === `none`) {
+        x.style.display = `block`;
     } else {
-        x.style.display = "none";
+        x.style.display = `none`;
     }
     z = ask();
-    g = "cups";
+    g = `cups`;
     if(z == 1){
-        g = "cup";
+        g = `cup`;
     }
     h = z*2;
-    document.getElementById("whiteWater").innerHTML = "Combine "+z+" "+g+" of rice with "+h.toFixed(2)+" cups of water and 1 Tbsp olive oil.";
-}
+    document.getElementById(`whiteWater`).innerHTML = `Combine `+z+` `+g+` of rice with `+h.toFixed(2)+` cups of water and 1 Tbsp olive oil.`;
+  }
 
-function CaliRice() {
-    let x = document.getElementById("CaliRice");
-    let y = document.getElementById("whiteRice");
-    y.style.display = "none";
-    if (x.style.display === "none") {
-        x.style.display = "block";
+  function CaliRice() {
+    let x = document.getElementById(`CaliRice`);
+    let y = document.getElementById(`whiteRice`);
+    y.style.display = `none`;
+    if (x.style.display === `none`) {
+        x.style.display = `block`;
     } else {
-        x.style.display = "none";
+        x.style.display = `none`;
     }
     z = ask();
-    g = "cups";
+    g = `cups`;
     if(z == 1){
-        g = "cup";
+        g = `cup`;
     }
     h = z*1.6;
-    document.getElementById("CaliWater").innerHTML = "Combine "+z+" "+g+" of rice with "+h.toFixed(2)+" cups of water "+
-    "or broth and 1 Tbsp olive oil.";
-}
+    document.getElementById(`CaliWater`).innerHTML = `Combine `+z+` `+g+` of rice with `+h.toFixed(2)+` cups of water `+
+    `or broth and 1 Tbsp olive oil.`;
+  }
 
-function ask() {
-    let z = prompt("How many cups of rice do you want?");
+  function ask() {
+    let z = prompt(`How many cups of rice do you want?`);
     z = parseFloat(z,10);
     while(isNaN(z) == true || z <= 0){
-        z = prompt("How many cups of rice do you want?");
+        z = prompt(`How many cups of rice do you want?`);
     }
     return z;
-}
+  }
